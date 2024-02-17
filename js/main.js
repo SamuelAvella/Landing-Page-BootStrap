dca779c09b4dace4f6c1fc057b62885c5aff6446
SamuelAvella <saveper1907@g.educaand.es>
2024-02-17 18:30:43 +0100

    fefef

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*****funcion escribir y borrar lo escrito para mas creatividad*******/
 let app = document.getElementById('typewriter');
  
 let typewriter = new Typewriter(app, {
@@ -16,35 +17,26 @@ typewriter
 
 function validacion(){
   let correcto = true;
-  let nombre=document.getElementById('nombre').value;//getter
-  let apellidos=document.getElementById('apellidos').value;//getter
+  let nombre=document.getElementById('nombre').value;
   let email=document.getElementById('email').value;
   let provincia=document.getElementById('provincia').value;
 
   if(nombre=="" || /^\s$/.test(nombre) || ! /^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]{2,}$/.test(nombre)){
       document.getElementById('nombreHelp').style.visibility="visible";
-      document.getElementById('nombre').style.borderColor="red";//en JS todos los atributos se llaman igual sin el guion. Ej: border-color -> borderColor
+      document.getElementById('nombre').style.borderColor="red";
       correcto = false;
   }
-  
-  
-  if(apellidos=="" || /^\s$/.test(apellidos) || ! /^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]{2,}$/.test(apellidos)){
-      document.getElementById('apellidosHelp').style.visibility="visible";
-      document.getElementById('apellidos').style.borderColor="red";
-      correcto = false;
-  }
-
   
   if(email=="" || ! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/.test(email)){
-      document.getElementById('emailHelp').style.visibility="visible";//setter
-      document.getElementById('email').style.borderColor="red";//setter
+      document.getElementById('emailHelp').style.visibility="visible";
+      document.getElementById('email').style.borderColor="red";
       correcto = false;
   }
 
   
  if(provincia==0){
-      document.getElementById('provinciaHelp').style.visibility="visible";//setter
-      document.getElementById('provincia').style.borderColor="red";//setter
+      document.getElementById('provinciaHelp').style.visibility="visible";
+      document.getElementById('provincia').style.borderColor="red";
       correcto = false;
   }
    
